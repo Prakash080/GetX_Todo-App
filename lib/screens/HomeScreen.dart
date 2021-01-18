6bd466e2c9d53fbb167cf298ce805b05f0ab7304
Prakash H <hprakash080@gmail.com>
2021-01-18 09:01:44 +0530

    Changes Done

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -6,66 +6,83 @@ import 'package:getx_todo_app/screens/TodoScreen.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    DateTime now = new DateTime.now();
     final TodoController todoController = Get.put(TodoController());
+
     return Scaffold(
         appBar: AppBar(
-          title: Text('GetX Todo List'),
+          backgroundColor: Colors.deepPurple,
+          title: Text(
+            'Todo List',
+            style: TextStyle(fontFamily: 'Spartan', fontSize: 28.0),
+          ),
+          centerTitle: true,
         ),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
+          backgroundColor: Colors.pink,
           onPressed: () {
             Get.to(TodoScreen());
           },
         ),
         body: Container(
+          padding: EdgeInsets.only(left: 10, right: 10, top: 5),
           child: Obx(() => ListView.separated(
-              itemBuilder: (context, index) => Dismissible(
-                    key: UniqueKey(),
-                    onDismissed: (_) {
-                      var removed = todoController.todos[index];
-                      todoController.todos.removeAt(index);
-                      Get.snackbar('Task removed',
-                          'The task "${removed.text}" was successfully removed.',
-                          mainButton: FlatButton(
-                            child: Text('Undo'),
-                            onPressed: () {
-                              if (removed.isNull) {
-                                return;
-                              }
-                              todoController.todos.insert(index, removed);
-                              removed = null;
-                              if (Get.isSnackbarOpen) {
-                                Get.back();
-                              }
-                            },
-                          ));
-                    },
-                    child: ListTile(
-                      title: Text(todoController.todos[index].text,
-                          style: (todoController.todos[index].done)
-                              ? TextStyle(
-                                  color: Colors.red,
-                                  decoration: TextDecoration.lineThrough)
-                              : TextStyle(
-                                  color: Theme.of(context)
-                                      .textTheme
-                                      .bodyText1
-                                      .color)),
-                      onTap: () {
-                        Get.to(TodoScreen(
-                          index: index,
-                        ));
+              itemBuilder: (context, index) => ListTile(
+                    title: Text(todoController.todos[index].text,
+                        style: (todoController.todos[index].done)
+                            ? TextStyle(
+                                color: Colors.pink,
+                                decoration: TextDecoration.lineThrough,
+                                fontFamily: 'Spartan',
+                                fontSize: 18.0,
+                              )
+                            : TextStyle(
+                                color:
+                                    Theme.of(context).textTheme.bodyText1.color,
+                                fontFamily: 'Spartan',
+                                fontSize: 18.0,
+                              )),
+                    leading: Checkbox(
+                      activeColor: Colors.deepPurple,
+                      value: todoController.todos[index].done,
+                      onChanged: (v) {
+                        var changed = todoController.todos[index];
+                        changed.done = v;
+                        todoController.todos[index] = changed;
                       },
-                      leading: Checkbox(
-                        value: todoController.todos[index].done,
-                        onChanged: (v) {
-                          var changed = todoController.todos[index];
-                          changed.done = v;
-                          todoController.todos[index] = changed;
-                        },
-                      ),
-                      trailing: Icon(Icons.chevron_right),
                     ),
+                    subtitle:
+                        Text('Created on:${now.day}/${now.month}/${now.year}'),
+                    trailing: IconButton(
+                        icon: Icon(todoController.todos[index].done
+                            ? Icons.delete_forever
+                            : Icons.edit),
+                        onPressed: () {
+                          if (todoController.todos[index].done) {
+                            var removed = todoController.todos[index];
+                            todoController.todos.removeAt(index);
+                            Get.snackbar('Todo removed',
+                                'The todo "${removed.text}" was successfully removed.',
+                                mainButton: FlatButton(
+                                  child: Text('Undo'),
+                                  onPressed: () {
+                                    if (removed.isNull) {
+                                      return;
+                                    }
+                                    todoController.todos.insert(index, removed);
+                                    removed = null;
+                                    if (Get.isSnackbarOpen) {
+                                      Get.back();
+                                    }
+                                  },
+                                ));
+                          } else {
+                            Get.to(TodoScreen(
+                              index: index,
+                            ));
+                          }
+                        }),
                   ),
               separatorBuilder: (_, __) => Divider(),
               itemCount: todoController.todos.length)),
--- lib/screens/TodoScreen.dart
+++ lib/screens/TodoScreen.dart
@@ -17,55 +17,60 @@ class TodoScreen extends StatelessWidget {
     }
     TextEditingController textEditingController =
         TextEditingController(text: text);
-    return Scaffold(
-      body: Container(
-        padding: EdgeInsets.all(12.0),
-        child: Column(
-          children: [
-            Expanded(
-              child: TextField(
-                controller: textEditingController,
-                autofocus: true,
-                decoration: InputDecoration(
-                    hintText: 'What do you want to accomplish?',
-                    border: InputBorder.none,
-                    focusedBorder: InputBorder.none),
-                style: TextStyle(
-                  fontSize: 25.0,
-                ),
-                keyboardType: TextInputType.multiline,
-                maxLines: 999,
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurple,
+          title: Text(
+            todoController.todos != null ? 'Edit Todo' : 'New Todo',
+            style: TextStyle(fontFamily: 'Spartan', fontSize: 24.0),
+          ),
+          centerTitle: true,
+        ),
+        body: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                  maxLines: 5,
+                  controller: textEditingController,
+                  autofocus: true,
+                  decoration: InputDecoration(
+                    labelText: 'Enter todo here',
+                    labelStyle: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'Spartan',
+                        fontSize: 14.0),
+                    contentPadding: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 20.0),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                            color: Colors.deepPurple, width: 2.0),
+                        borderRadius: BorderRadius.circular(10.0)),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  )),
+              new Padding(padding: EdgeInsets.only(top: 10)),
+              RaisedButton(
+                child: Text((this.index.isNull) ? 'ADD' : 'UPDATE',
+                    style: TextStyle(fontFamily: 'Spartan', fontSize: 18.0)),
+                color: Colors.pink,
+                onPressed: () {
+                  if (this.index.isNull) {
+                    todoController.todos
+                        .add(Todo(text: textEditingController.text));
+                  } else {
+                    var editing = todoController.todos[index];
+                    editing.text = textEditingController.text;
+                    todoController.todos[index] = editing;
+                  }
+                  Get.back();
+                },
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                RaisedButton(
-                  child: Text('Cancel'),
-                  color: Colors.red,
-                  onPressed: () {
-                    Get.back();
-                  },
-                ),
-                RaisedButton(
-                  child: Text((this.index.isNull) ? 'Add' : 'Edit'),
-                  color: Colors.green,
-                  onPressed: () {
-                    if (this.index.isNull) {
-                      todoController.todos
-                          .add(Todo(text: textEditingController.text));
-                    } else {
-                      var editing = todoController.todos[index];
-                      editing.text = textEditingController.text;
-                      todoController.todos[index] = editing;
-                    }
-
-                    Get.back();
-                  },
-                ),
-              ],
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
